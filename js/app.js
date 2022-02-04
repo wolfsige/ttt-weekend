f7a9252f269b0cd49ef71a0b1e247399c097e977
Wolf <d.wolftripp@gmail.com>
2022-02-04 08:53:20 -0800

    add cashed element referances

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
-/*-------------------------------- Constants --------------------------------*/
+document.getElementById/*-------------------------------- Constants --------------------------------*/
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-squareArray = []
+squareArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 let p1Turn, p2Turn 
 
-let winner, tie
+let winner, tie, inPlay
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const square = document.getElementsByClassName('sqr')
+square1 = document.getElementById('sq0')
+square2 = document.getElementById('sq1')
+square3 = document.getElementById('sq2')
+square4 = document.getElementById('sq3')
+square5 = document.getElementById('sq4')
+square6 = document.getElementById('sq5')
+square7 = document.getElementById('sq6')
+square8 = document.getElementById('sq7')
+square9 = document.getElementById('sq8')
+const gStatus = document.querySelector('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
