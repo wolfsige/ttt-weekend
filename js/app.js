9c50d0652a4d71a6a29f96c13944d8d8d4d92979
Wolf <d.wolftripp@gmail.com>
2022-02-04 14:16:42 -0800

    add init stub-up

--- js/app.js
+++ js/app.js
@@ -1,26 +1,27 @@
-document.getElementById/*-------------------------------- Constants --------------------------------*/
+/*-------------------------------- Constants --------------------------------*/
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 squareArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-let p1Turn, p2Turn 
+let p1Turn = 1
+let p2Turn = -1
 
-let winner, tie, inPlay
+let isWinner, tie, inPlay
 
 
 /*------------------------ Cached Element References ------------------------*/
-const square = document.getElementsByClassName('sqr')
-const square1 = document.getElementById('sq0')
-const square2 = document.getElementById('sq1')
-const square3 = document.getElementById('sq2')
-const square4 = document.getElementById('sq3')
-const square5 = document.getElementById('sq4')
-const square6 = document.getElementById('sq5')
-const square7 = document.getElementById('sq6')
-const square8 = document.getElementById('sq7')
-const square9 = document.getElementById('sq8')
-const gStatus = document.querySelector('message')
+square = document.getElementsByClassName('sqr')
+square1 = document.getElementById('sq0')
+square2 = document.getElementById('sq1')
+square3 = document.getElementById('sq2')
+square4 = document.getElementById('sq3')
+square5 = document.getElementById('sq4')
+square6 = document.getElementById('sq5')
+square7 = document.getElementById('sq6')
+square8 = document.getElementById('sq7')
+square9 = document.getElementById('sq8')
+gStatus = document.querySelector('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -30,5 +31,27 @@ const gStatus = document.querySelector('message')
 /*-------------------------------- Functions --------------------------------*/
 init()
 function init() {
-
+  square1 = squareArray[0]
+  square2 = squareArray[1]
+  square3 = squareArray[2]
+  square4 = squareArray[3]
+  square5 = squareArray[4]
+  square6 = squareArray[5]
+  square7 = squareArray[6]
+  square8 = squareArray[7]
+  square9 = squareArray[8]
+  p1Turn = true
+  isWinner = false
+ render()
+//  console.log()
 }
+
+function render(){
+  squareArray.forEach(squareSet => {
+    
+    // console.log()
+  });
+}
+
+// 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
+		  // 3.3.1.2) Style that square however you wish dependant on the value contained in the current cell being iterated over (-1, 1, or null)
