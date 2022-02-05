2e4a38e6f1b1e7ab9bd41701b2502c26ffb1a1be
Wolf <d.wolftripp@gmail.com>
2022-02-05 13:48:43 -0800

    add class square

--- js/app.js
+++ js/app.js
@@ -3,15 +3,12 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-squareArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-let p1Turn = 1
-let p2Turn = -1
-
-let isWinner, tie, inPlay
+let squareArray = [], isWinner, tie, turnOrder
 
 
 /*------------------------ Cached Element References ------------------------*/
-square = document.getElementsByClassName('sqr')
+theBoard = document.getElementsByClassName('board')
+gStatus = document.querySelector('#message')
 square1 = document.getElementById('sq0')
 square2 = document.getElementById('sq1')
 square3 = document.getElementById('sq2')
@@ -21,7 +18,6 @@ square6 = document.getElementById('sq5')
 square7 = document.getElementById('sq6')
 square8 = document.getElementById('sq7')
 square9 = document.getElementById('sq8')
-gStatus = document.querySelector('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -31,6 +27,7 @@ gStatus = document.querySelector('message')
 /*-------------------------------- Functions --------------------------------*/
 init()
 function init() {
+  squareArray = [null, null, null, null, null, null, null, null, null]
   square1 = squareArray[0]
   square2 = squareArray[1]
   square3 = squareArray[2]
@@ -40,18 +37,27 @@ function init() {
   square7 = squareArray[6]
   square8 = squareArray[7]
   square9 = squareArray[8]
-  p1Turn = true
-  isWinner = false
+  turnOrder = 1
+  isWinner = null
  render()
-//  console.log()
+
 }
 
 function render(){
-  squareArray.forEach(squareSet => {
+  forEach(function(squareArray, index){
     
-    // console.log()
-  });
+  })
 }
 
-// 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
+
+
+// 3.3) The render function should:
+	  // 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:
+		  // 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
 		  // 3.3.1.2) Style that square however you wish dependant on the value contained in the current cell being iterated over (-1, 1, or null)
+	  // 3.3.2) Render a message reflecting the currrent game state:
+	    // 3.3.2.1) If winner has a value other than null (game still in progress), render whose turn it is.
+	      // Hint: Maybe use a ternary inside of a template literal here?
+	    // 3.3.2.2) If winner is equal to 'T' (tie), render a tie message.
+	    // 3.3.2.3) Otherwise, render a congratulatory message to which player has won.
+	      // Hint (again): Maybe use a ternary inside a template literal here
